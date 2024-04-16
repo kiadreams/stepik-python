7316e71f3ee4ace49dc3fde2a365e5cdeaa7d5fa
Igor Kobelev <kia1809@yandex.ru>
2024-04-16 08:46:14 +0300

    started new course

---
+++ good_good_python/simple.py
@@ -0,0 +1,16 @@
+# \n = 5
+# don't = 5
+_b = 5
+__arg_c__ = 5
+TT1 = 5
+# 25d = 5
+d25 = 5
+S = 5
+# 7_b = 5
+var_a = 5
+print(__arg_c__)
+type = 7
+print(type)
+print(min(1, 2))
+print(round(50.445, -1))
+print(f'result {S = }')
