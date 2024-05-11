581a4bd353cbac98848455861091822a224714eb
Igor Kobelev <kia1809@yandex.ru>
2024-05-11 11:14:23 +0300

    added modules black and flake8

--- all_tests.py
+++ all_tests.py
@@ -3,10 +3,9 @@ from contextlib import redirect_stdout
 
 from mytests import py_gen_tests as pt
 
-
 text_io = io.StringIO()
 with redirect_stdout(text_io):
-    pt.test_count_args(*[2, 3, [], '!'])
-
-result = text_io.getvalue()
-assert int(result) == 4
+    pt.test_count_args(2, 3, [], "!")
+    text = text_io.getvalue()
+if int(text) != 4:
+    raise ValueError("Expected positive value.")
