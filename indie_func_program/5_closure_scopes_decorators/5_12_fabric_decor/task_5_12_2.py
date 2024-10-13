44d874051d7bc212717e374699cf4670b03a8479
Igor Kobelev <kia1809@yandex.ru>
2024-10-13 20:25:05 +0300

    plus one more task of 5.12 section

---
+++ indie_func_program/5_closure_scopes_decorators/5_12_fabric_decor/task_5_12_2.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+
+def limit_query(n):
+    def decorator(func):
+        limit = 0
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            nonlocal limit
+            limit += 1
+            if limit > n:
+                print(f'Лимит вызовов закончен, все {n} попытки израсходованы')
+                return None
+            return func(*args, **kwargs)
+
+        return inner
+
+    return decorator
