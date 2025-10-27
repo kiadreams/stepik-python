88efc4981fa8bfe61ed31980700a8f86bdf8ed50
Igor Kobelev <kia1809@yandex.ru>
2025-10-27 18:43:10 +0300

    logging part 1

---
+++ python_charge_your_batteries/6_logging/example_logging.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug("Это сообщение уровня DEBUG")
+logging.info("Это сообщение уровня INFO")
+logging.warning("Это сообщение уровня WARNING")
+logging.error("Это сообщение уровня ERROR")
+logging.critical("Это сообщение уровня CRITICAL")
