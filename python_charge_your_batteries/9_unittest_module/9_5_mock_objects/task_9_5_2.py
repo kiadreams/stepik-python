2e57e8508fdef411c8e2ec01f6f16244be857c41
Igor K. <kia1809@yandex.ru>
2025-12-20 17:13:30 +0300

    change task_9_5_2

--- python_charge_your_batteries/9_unittest_module/9_5_mock_objects/task_9_5_2.py
+++ python_charge_your_batteries/9_unittest_module/9_5_mock_objects/task_9_5_2.py
@@ -2,9 +2,14 @@ import datetime
 from unittest import mock
 
 artyom_bd = datetime.date(1990, 8, 21)
-with mock.patch('datetime.date', wraps=datetime.date) as datetime.date:
-    print(datetime.date.today())  # Сегодняшняя дата
-    datetime.date.today.return_value = artyom_bd
-    print(datetime.date.today())  # ДР Артёма
-
+curr_today = datetime.date.today
+print(curr_today())
+curr_today = mock.Mock(return_value=artyom_bd)
+print(curr_today())
+# with mock.patch('datetime.date', wraps=datetime.date) as datetime.date:
+#     print(datetime.date.today())  # Сегодняшняя дата
+#     datetime.date.today.return_value = artyom_bd
+#     print(datetime.date.today())  # ДР Артёма
+print(curr_today())
+print(curr_today())
 print(datetime.date.today())  # Всё равно сегодняшняя дата
